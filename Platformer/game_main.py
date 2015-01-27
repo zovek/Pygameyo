8c647f9bf04510740e91acf54d91b0f92c17e525
mrivnak <mrivnak86@gmail.com>
2015-01-27 18:49:20 -0500

    implemented configuration file

--- Platformer/game_main.py
+++ Platformer/game_main.py
@@ -1,7 +1,7 @@
 #version 0.0.01a
 
 #imports
-import pygame, random, math, sys
+import pygame, random, math, sys, configparser
 from pygame.locals import *
 pygame.init()
 
@@ -12,43 +12,35 @@ Red   = (255,  0,  0)
 Green = (  0,255,  0)
 Blue  = (  0,  0,255)
 
+#ConfigParser
+config = configparser.ConfigParser()
+config.read('config.ini')
+def configSectionMap(section):
+    dict1 = {}
+    options = config.options(section)
+    for option in options:
+        try:
+            dict1[option] = config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
 #resolution settings
-aspectRatio = aspectRatio1
-res = res1
-fullscreen = (False)
-aspectRatio1 = ('16:9')
-aspectRatio2 = ('16:10')
-if aspectRatio == aspectRatio1:
-	if res == res1:
-		xres = (1280)
-		yres = (720)
-		resScale = (1)
-	elif res == res2:
-		xres = (1920)
-		yres = (1080)
-		resScale = (1.5)
-	elif res == res3:
-		xres = (2560)
-		yres = (1440)
-		resScale = (2)
-	elif res == res4:
-		xres = (3840)
-		yres = (2160)
-		resScale = (3)
-elif aspectRatio == aspectRatio2:
-	if res == res1:
-		xres = (1280)
-		yres = (800)
-		resScale = (1)
-	elif res == res2:
-		xres = (1920)
-		yres = (1200)
-		resScale = (1.5)
-	elif res == res3:
-		xres = (2560)
-		yres = (1600)
-		resScale = (2)
-	elif res == res4:
-		xres = (3840)
-		yres = (2400)
-		resScale = (3)
+xres = int(configSectionMap('Display') ['horizontalres'])
+yres = int(configSectionMap('Display') ['verticalres'])
+
+#window settings
+fullscreenSet = configSectionMap('Display')['fullscreen']
+if fullscreenSet == True:
+	screen = pygame.display.set_mode((xres, yres), FULLSCREEN, DOUBLEBUF)
+else:
+	screen = pygame.display.set_mode((xres, yres), DOUBLEBUF)
+
+#sprite classes
+#class Player(pygame.sprite.Sprite):
+
+#class Enemy(pygame.sprite.Sprite):
+
